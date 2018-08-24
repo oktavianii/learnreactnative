57220e8952fb5780f502bce5ae004a7cf06d6ed3
oktavianii <oktaviani617@gmail.com>
2018-08-24 15:29:51 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Nama  : Widya Oktaviani</Text>
+        <Text>Kelas : XI RPL 1</Text>
+        <Text>Absen : 39</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
